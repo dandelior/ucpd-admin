ceff7fdf9a64e691362c2a994b9eeb0863a29ac5
Juan Berrios <jberrioscifuentes@gmail.com>
2022-09-28 20:21:39 -0300

    fix read access

--- src/collections/AskThePastor.ts
+++ src/collections/AskThePastor.ts
@@ -1,55 +1,82 @@
-import { CollectionConfig } from 'payload/types';
-import formatSlug from '../utilities/formatSlug';
+import { CollectionConfig, FieldHook } from "payload/types";
+import formatSlug from "../utilities/formatSlug";
 
 const AskThePastor: CollectionConfig = {
-    slug: 'askthepastor',
-    labels: {
-        singular: 'Pregunta al pastor',
-        plural: 'Preguntas al pastor'
+  slug: "askthepastor",
+  labels: {
+    singular: "Pregunta al pastor",
+    plural: "Preguntas al pastor",
+  },
+  access: {
+    read: (): boolean => true,
+  },
+  fields: [
+    {
+      name: "title",
+      type: "text",
+      label: "Titulo/Pregunta",
+      required: true,
     },
-    fields: [
-        {
-            name: 'title',
-            type: 'text',
-            label: 'Titulo/Pregunta',
-            required: true,
-        },
-        {
-            name: 'date',
-            type: 'date',
-            label: 'Fecha',
-            defaultValue: Date,
-            required: true,
-        },
-        {
-            name: 'content',
-            type: 'richText',
-            label: 'Contenido/Respuesta',
-            required: true,
-        },
-        {
-            name: 'slug',
-            label: 'Slug',
-            type: 'text',
-            admin: {
-                position: 'sidebar',
-                description: 'Dejar en blanco para generarlo automáticamente al guardar',
-            },
-            hooks: {
-                beforeValidate: [
-                    formatSlug('title'),
-                ],
-            },
-        }
-    ],
-    admin: {
-        useAsTitle: 'title',
+    {
+      name: "date",
+      type: "date",
+      label: "Fecha",
+      defaultValue: Date,
+      required: true,
     },
-    versions: {
-        drafts: {
-          autosave: true,
-        }
+    {
+      name: "featured_image",
+      type: "upload",
+      label: "Imagen destacada",
+      relationTo: "imagenes",
+      //   unique: true,
+      required: true,
     },
-}
+    {
+      name: "content",
+      type: "richText",
+      label: "Contenido/Respuesta",
+      required: true,
+    },
+    {
+      name: "slug",
+      label: "Slug",
+      type: "text",
+      admin: {
+        position: "sidebar",
+        description:
+          "Dejar en blanco para generarlo automáticamente al guardar",
+      },
+      hooks: {
+        beforeValidate: [formatSlug("title")],
+      },
+    },
+    {
+      name: "num",
+      label: "Número",
+      type: "number",
+      unique: true,
+      admin: {
+        position: "sidebar",
+        readOnly: true,
+      },
+      hooks: {
+        beforeChange: [
+          (args) => {
+            return (args.data.num = 222);
+          },
+        ],
+      },
+    },
+  ],
+  admin: {
+    useAsTitle: "title",
+  },
+  versions: {
+    drafts: {
+      autosave: true,
+    },
+  },
+};
 
 export default AskThePastor;
--- src/collections/Videos.ts
+++ src/collections/Videos.ts
@@ -1,70 +1,91 @@
-import { CollectionConfig } from 'payload/types';
-import formatSlug from '../utilities/formatSlug';
+import { CollectionConfig } from "payload/types";
+import formatSlug from "../utilities/formatSlug";
 
 const Videos: CollectionConfig = {
-    slug: 'videos',
-    labels: {
-        singular: 'Video',
-        plural: 'Videos',
+  slug: "videos",
+  labels: {
+    singular: "Video",
+    plural: "Videos",
+  },
+  access: {
+    read: (): boolean => true,
+  },
+  fields: [
+    {
+      name: "title",
+      type: "text",
+      label: "Título",
+      required: true,
     },
-    fields: [
+    {
+      type: "row",
+      fields: [
         {
-            name: 'title',
-            type: 'text',
-            label: 'Título',
-            required: true,
+          name: "date",
+          type: "date",
+          label: "Fecha",
+          defaultValue: Date,
+          required: true,
         },
         {
-            name: 'date',
-            type: 'date',
-            label: 'Fecha',
-            defaultValue: Date,
-            required: true,
+          name: "author",
+          type: "relationship",
+          relationTo: "authors",
+          label: "Autor",
         },
-        {
-            name: 'videoLink',
-            type: 'text',
-            label: 'Link del video',
-            required: true,
-        },
-        {
-            name: 'description',
-            type: 'textarea',
-            label: 'Descripción',
-            required: true,
-        },
-        {
-            name: 'serie',
-            type: 'relationship',
-            relationTo: 'series',
-            label: 'Serie del Video',
-            admin: {
-                position: 'sidebar',
-            }
-        },
-        {
-            name: 'slug',
-            label: 'Slug',
-            type: 'text',
-            admin: {
-                position: 'sidebar',
-                description: 'Dejar en blanco para generarlo automáticamente al guardar',
-            },
-            hooks: {
-                beforeValidate: [
-                    formatSlug('title'),
-                ],
-            },
-        }
-    ],
-    admin: {
-        useAsTitle: 'title',
+      ],
     },
-    versions: {
-        drafts: {
-          autosave: true,
-        }
+    {
+      name: "featured_image",
+      type: "upload",
+      label: "Imagen destacada",
+      relationTo: "imagenes",
+      required: true,
     },
-}
+    {
+      name: "videoLink",
+      type: "text",
+      label: "Link del video",
+      required: true,
+    },
+    {
+      name: "description",
+      type: "textarea",
+      label: "Descripción",
+      required: true,
+    },
+    {
+      name: "serie",
+      type: "relationship",
+      relationTo: "series",
+      label: "Serie del Video",
+      hasMany: false,
+      admin: {
+        position: "sidebar",
+      },
+    },
+    {
+      name: "slug",
+      label: "Slug",
+      type: "text",
+      admin: {
+        position: "sidebar",
+        description:
+          "Dejar en blanco para generarlo automáticamente al guardar",
+      },
+      hooks: {
+        beforeValidate: [formatSlug("title")],
+      },
+    },
+  ],
+  admin: {
+    useAsTitle: "title",
+  },
+  versions: {
+    drafts: {
+      autosave: true,
+    },
+  },
+};
 
 export default Videos;
